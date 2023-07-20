04954ba76e1d85148d51f8bc744a9a8b8b8dcb16
Matthew "The ESC Wizard" Shephard <mshephard@idgc.us>
2023-07-20 12:48:10 -0500

    Create TimeOffRequestEntryMethod.cs

---
+++ Intuit.TSheets/Model/Enums/TimeOffRequestEntryMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Intuit.TSheets.Model.Enums
+{
+    public enum TimeOffRequestEntryMethod
+    {
+        /// <summary>
+        /// Manual time off request entries have a date and a duration (in seconds). 
+        /// </summary>
+        [EnumMember(Value = "manual")]
+        Manual,
+
+        /// <summary>
+        /// Regular time off request entries have a start/end time (duration is calculated by TSheets) for determining availability in Schedule.
+        /// </summary>
+        [EnumMember(Value = "regular")]
+        Regular
+    }
+}
