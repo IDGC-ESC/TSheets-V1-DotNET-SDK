fd5017213f94c6ebc25b5ebd6744629292349998
Matthew "The ESC Wizard" Shephard <mshephard@idgc.us>
2023-07-20 12:48:05 -0500

    Create TimeOffRequestStatus.cs

---
+++ Intuit.TSheets/Model/Enums/TimeOffRequestStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Intuit.TSheets.Model.Enums
+{
+    public enum TimeOffRequestStatus
+    {
+        [EnumMember(Value = "pending")]
+        Pending,
+
+        [EnumMember(Value = "approved")]
+        Approved,
+
+        [EnumMember(Value = "denied")]
+        Denied,
+
+        [EnumMember(Value = "canceled")]
+        Canceled
+    }
+}
