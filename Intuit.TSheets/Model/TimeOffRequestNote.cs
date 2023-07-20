d134e821a63900a8f08e7f6b7a321e2e453be5c7
Matthew "The ESC Wizard" Shephard <mshephard@idgc.us>
2023-07-20 11:12:49 -0500

    Create TimeOffRequestNote.cs

---
+++ Intuit.TSheets/Model/TimeOffRequestNote.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Intuit.TSheets.Client.Serialization.Attributes;
+using Intuit.TSheets.Client.Serialization.Converters;
+using Newtonsoft.Json;
+
+namespace Intuit.TSheets.Model
+{
+    public class TimeOffRequestNote : IIdentifiable
+    {
+        /// <summary>
+        /// Gets the id of the time off request.
+        /// </summary>
+        [NoSerializeOnCreate]
+        [JsonProperty("id")]
+        public long Id { get; internal set; }
+
+        [JsonProperty("time_off_request_id")]
+        public IReadOnlyList<long> RequestID { get; set; } = Array.Empty<long>();
+
+        /// <summary>
+        /// Gets or sets the id for the user that this timesheet belongs to.
+        /// </summary>
+        [JsonProperty("user_id")]
+        public long UserID { get; internal set; }
+
+        [JsonProperty("active")]
+        public bool Active { get; internal set; }
+
+        [JsonProperty("note")]
+        public string Note { get; set; } = null!;
+
+        [JsonConverter(typeof(DateTimeFormatConverter))]
+        [JsonProperty("created")]
+        public DateTimeOffset Created { get; internal set; }
+
+        [JsonConverter(typeof(DateTimeFormatConverter))]
+        [JsonProperty("last_modified")]
+        public DateTimeOffset LastModified { get; internal set; }
+    }
+}
