c07b4797d6bda69aee42b93f76b9329cb7fb1435
Matthew "The ESC Wizard" Shephard <mshephard@idgc.us>
2023-08-02 11:33:23 -0500

    Create QBTimeResponseData.cs

---
+++ Intuit.TSheets/Api/QBTimeResponseData.cs
@@ -0,0 +1,23 @@
+﻿namespace Intuit.TSheets.Api
+{
+    public class QBTimeResponseData<T>
+    {
+        public T Value { get; set; }
+        public ResultsMeta ResultsMeta { get; set; }
+
+        public static explicit operator QBTimeResponseData<T>((T, ResultsMeta) apiResponse)
+        {
+            return new()
+            {
+                Value = apiResponse.Item1,
+                ResultsMeta = apiResponse.Item2
+            };
+        }
+
+        public static QBTimeResponseData<TResult> FromResponse<TResult>((TResult, ResultsMeta) response) => new()
+        {
+            Value = response.Item1,
+            ResultsMeta = response.Item2
+        };
+    }
+}
